8148c699a0b92d26ae6c150f2ff6a576c071ca82
Silky Ng <silky.ng@gmail.com>
2021-10-14 08:49:35 +0100

    added insertAfter message

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+var msgID = 1;
+
 function sendMessage(){
-    var userMsg = $('#userInput').val();
-    $('h2').text(userMsg);
-    $('#userInput').val('');
+    var userMsg = $('#userInput').val(); //Get user message
+    $('#userInput').val(''); //Clear text field
+    
+    var userMsgID = msgID + 1;
+    var messageContent = "<div id =" + userMsgID.toString() + 
+    " class=\"computer\"><p>" + userMsg + "</p><p id= " + userMsgID.toString() + ">12/10/21 17:09</p></div>";
+
+    var computerMsgID =  "#" + msgID.toString();
+    $(messageContent).insertAfter($(computerMsgID));
+    msgID++;
 }
