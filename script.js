916060412f8ddfc4880c5bd827f64783cf350a8c
Silky Ng <silky.ng@gmail.com>
2021-10-14 11:19:55 +0100

    message ID order of conversation between user and computer

--- script.js
+++ script.js
@@ -9,18 +9,37 @@ function sendMessage(){
     const userMsg = $('#userInput').val(); //Get user message
     $('#userInput').val(''); //Clear text field
     
-    var userMsgID = msgID + 1;
+    msgID++;
     
-    const messageContent = "<div id =" + userMsgID.toString() + 
+    const messageContent = "<div id =" + msgID.toString() + 
     " class=\"user\"><p>" + userMsg + "</p><p id= \"dateTime\">" + getDateTime() + "</p></div>";
 
-    var computerMsgID =  "#" + msgID.toString();
+    var previousMsgID = msgID - 1;
+    var computerMsgID =  "#" + previousMsgID.toString();
     $(messageContent).insertAfter($(computerMsgID));
-    msgID++;
+    
+    setTimeout(generateRandomMsg, 3000);
 }
 
 function getDateTime(){
     const date = new Date();
     const msgDateTime = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + " " + date.getHours() + ":" + (date.getMinutes()<10? "0": "") + date.getMinutes();
     return msgDateTime.toString();
+}
+
+function generateRandomMsg(){
+    var messages = ["Great to hear!", "How's the weather?", "What are you up to?", "Where do you live?", "Where do you work?"];
+
+    var randNo = Math.floor(Math.random()*5); //Random int from 0-4
+
+    var message = messages[randNo];
+
+    msgID++;
+    const messageContent = "<div id =" + msgID.toString() + 
+    " class=\"computer\"><p>" + message + "</p><p id= \"dateTime\">" + getDateTime() + "</p></div>";
+
+    var previousMsgID = msgID -1;
+    var userMsgID = "#" + previousMsgID.toString();
+    $(messageContent).insertAfter($(userMsgID));
+    
 }
