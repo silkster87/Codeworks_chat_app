30a0286d8b4e091248b8cf953eb19f9905f6f7ce
Silky Ng <silky.ng@gmail.com>
2021-10-14 10:36:46 +0100

    added immediately invoked function for dateTime

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 var msgID = 1;
 
+
+(function intialDateTime(){
+    document.getElementById("dateTime").innerHTML = getDateTime();
+})(); //Immediate invoked to get the date time right now
+
 function sendMessage(){
-    var userMsg = $('#userInput').val(); //Get user message
+    const userMsg = $('#userInput').val(); //Get user message
     $('#userInput').val(''); //Clear text field
     
     var userMsgID = msgID + 1;
-    var messageContent = "<div id =" + userMsgID.toString() + 
-    " class=\"computer\"><p>" + userMsg + "</p><p id= " + userMsgID.toString() + ">12/10/21 17:09</p></div>";
+    
+    const messageContent = "<div id =" + userMsgID.toString() + 
+    " class=\"user\"><p>" + userMsg + "</p><p id= \"dateTime\">" + getDateTime() + "</p></div>";
 
     var computerMsgID =  "#" + msgID.toString();
     $(messageContent).insertAfter($(computerMsgID));
     msgID++;
+}
+
+function getDateTime(){
+    const date = new Date();
+    const msgDateTime = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + " " + date.getHours() + ":" + (date.getMinutes()<10? "0": "") + date.getMinutes();
+    return msgDateTime.toString();
 }
